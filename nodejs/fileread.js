0ee3537965a89c0591ce818edfac40aa1de16366
darudeeee <haode0916@naver.com>
2024-06-20 10:48:22 +0900

    설명 추가

--- nodejs/fileread.js
+++
@@ -1,8 +0,0 @@
-// npm start는 json에서 main으로 지정해뒀기 때문에
-// cmd창에서 경로 입력 후 node fileread.js 하면 됨
-
-var fs = require("fs");
-fs.readFile("sample.txt", "utf-8", function (err, data) {
-  // 읽고싶은 파일 명 = sample.txt
-  console.log(data);
-});
